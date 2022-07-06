29dad2fd122a787393b584e122493de2f67d7439
Joel <jcbluemel@yahoo.com>
2022-07-06 12:16:59 -0700

    added testing for helpers/sql.js

--- helpers/sql.js
+++ helpers/sql.js
@@ -3,18 +3,22 @@
 const { BadRequestError } = require("../expressError");
 
 /** Takes JSON data and prepares it for SQL queries
- * 
+ *
  * If no data given, return 400 Error
- * 
+ *
  * Given {"firstName": "Aliya", "age": 32} =>
- * 
+ *
  * Returns {
  *    setCols: '"first_name"=$1, "age"=$2',
  *    values: ["Aliya", 32]
  *  }
 */
 
+//TODO: Should we add an error check for not supplying the jsToSql?
+
 function sqlForPartialUpdate(dataToUpdate, jsToSql) {
+  console.log("sqlForPartialUpdate", dataToUpdate, jsToSql);
+
   const keys = Object.keys(dataToUpdate);
   if (keys.length === 0) throw new BadRequestError("No data");
 
--- helpers/sql.test.js
+++ helpers/sql.test.js
@@ -1,2 +1,38 @@
-const { createToken } = require("./sql");
-const { SECRET_KEY } = require("../config");
+"use strict";
+
+const { sqlForPartialUpdate } = require("./sql");
+const { BadRequestError } = require("../expressError");
+
+
+describe("sqlForPartialUpdate", function () {
+  test("works", function () {
+    const { setCols, values } = sqlForPartialUpdate(
+      { firstName: "Test", age: 42 },
+      { firstName: "first_name"});
+    expect(setCols).toEqual('"first_name"=$1, "age"=$2');
+    expect(values).toEqual(["Test", 42]);
+  });
+
+  test("no data failure", function () {
+    try {
+      sqlForPartialUpdate(
+        {},
+        { firstName: "first_name"});
+      fail();
+    } catch (err) {
+      expect(err instanceof BadRequestError).toBeTruthy();
+    }
+  });
+
+  //TODO: Test for potential error we would need to add to sql.js
+  // test("no jsToSql failure", function () {
+  //   try {
+  //     sqlForPartialUpdate(
+  //       { firstName: "Test", age: 42 },
+  //       {});
+  //     fail();
+  //   } catch (err) {
+  //     expect(err instanceof BadRequestError).toBeTruthy();
+  //   }
+  // });
+});

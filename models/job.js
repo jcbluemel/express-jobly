63ca09144bd6c786c2c333531fcffaf74e50b399
Joel <jcbluemel@yahoo.com>
2022-07-07 16:59:16 -0700

    added create to job model and finished its tests

---
+++ models/job.js
@@ -0,0 +1,48 @@
+"use strict";
+
+const db = require("../db");
+const { BadRequestError, NotFoundError } = require("../expressError");
+
+/** Related functions for companies. */
+
+class Job {
+  /** Create a job (from data), update db, return new job data.
+   * //TODO: schema for equity should be string. check in route if NaN
+   * data should be { title, salary, equity, company_handle }
+   *
+   * Returns { id, title, salary, equity, company_handle }
+   * */
+
+   static async create({ title, salary, equity, company_handle }) {
+    const noCompany = await db.query(
+      `SELECT handle
+           FROM companies
+           WHERE handle = $1`,
+      [company_handle]);
+
+    if (noCompany.rows.length === 0)
+      throw new BadRequestError(`Company doesn't exist: ${company_handle}`);
+
+    const result = await db.query(
+      `INSERT INTO jobs(
+          title,
+          salary,
+          equity,
+          company_handle)
+           VALUES
+             ($1, $2, $3, $4)
+           RETURNING id, title, salary, equity, company_handle`,
+      [
+        title,
+        salary,
+        equity,
+        company_handle,
+      ],
+    );
+    const job = result.rows[0];
+
+    return job;
+  }
+}
+
+module.exports = Job;
--- models/job.test.js
+++ models/job.test.js
@@ -21,23 +21,30 @@ describe("create", function () {
   const newJob = {
     title: "new",
     salary: 40000,
-    equity: 0.04,
+    equity: "0.04",
     company_handle: "c3",
   };
 
   test("works", async function () {
     let job = await Job.create(newJob);
-    expect(job).toEqual(newJob);
+    expect(job).toEqual({
+      id: expect.any(Number),
+      title: "new",
+      salary: 40000,
+      equity: "0.04",
+      company_handle: "c3",
+    });
 
     const result = await db.query(
-          `SELECT title, salary, equity, company_handle
-           FROM jobs
-           WHERE company_handle = 'c3'`);
+      `SELECT id, title, salary, equity, company_handle
+        FROM jobs
+        WHERE company_handle = 'c3'`);
     expect(result.rows).toEqual([
       {
+        id: expect.any(Number),
         title: "new",
         salary: 40000,
-        equity: 0.04,
+        equity: "0.04",
         company_handle: "c3",
       },
     ]);
@@ -47,20 +54,14 @@ describe("create", function () {
     const newJobFakeCompany = {
       title: "new",
       salary: 40000,
-      equity: 0.04,
+      equity: "0.04",
       company_handle: "no-company",
     };
     try {
       await Job.create(newJobFakeCompany);
-      fail();
+      throw new Error("Fail test, you shouldn't get here")
     } catch (err) {
       expect(err instanceof BadRequestError).toBeTruthy();
     }
-
-    // const result = await db.query(
-    //       `SELECT title, salary, equity, company_handle
-    //        FROM jobs
-    //        WHERE company_handle = 'foo'`);
-    // expect(result.rows).toEqual([]);
   });
 });

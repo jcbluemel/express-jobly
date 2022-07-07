0b4b26ccc1b182b690143037e47a4901ada0e117
tdhood <taylordannhood@gmail.com>
2022-07-07 12:32:46 -0700

    saving

--- middleware/auth.js
+++ middleware/auth.js
@@ -22,7 +22,7 @@ function authenticateJWT(req, res, next) {
       const token = authHeader.replace(/^[Bb]earer /, "").trim();
       res.locals.user = jwt.verify(token, SECRET_KEY);
     }
-    console.log("AKJWHKDJAHWKJHAWKJHKJHAWD", res.locals.user.isAdmin);
+    console.log("AKJWHKDJAHWKJHAWKJHKJHAWD", res.locals.user);
     return next();
   } catch (err) {
     return next();
@@ -57,9 +57,22 @@ function ensureLoggedIn(req, res, next) {
   }
 }
 
+function ensureAdminOrCurrentUser(req, res, next) {
+  console.log("ensureAdminOrCurr++++++++++++++++")
+  console.log('username', res.locals.user.username)
+  console.log('curUser', req.params.username)
+  try {
+    if (res.locals.user.username !== req.params.username || !res.locals.user.isAdmin) throw new UnauthorizedError("THIS SUCKS");
+    return next();
+  } catch (err) {
+    return next(err);
+  }
+}
+
 
 module.exports = {
   authenticateJWT,
   ensureLoggedIn,
-  ensureAdmin
+  ensureAdmin,
+  ensureAdminOrCurrentUser
 };
--- routes/users.js
+++ routes/users.js
@@ -5,7 +5,11 @@
 const jsonschema = require("jsonschema");
 
 const express = require("express");
-const { ensureLoggedIn } = require("../middleware/auth");
+const {
+  ensureLoggedIn,
+  ensureAdmin,
+  ensureAdminOrCurrentUser,
+} = require("../middleware/auth");
 const { BadRequestError } = require("../expressError");
 const User = require("../models/user");
 const { createToken } = require("../helpers/tokens");
@@ -13,7 +17,6 @@ const userNewSchema = require("../schemas/userNew.json");
 const userUpdateSchema = require("../schemas/userUpdate.json");
 
 const router = express.Router();
-
 
 /** POST / { user }  => { user, token }
  *
@@ -24,17 +27,15 @@ const router = express.Router();
  * This returns the newly created user and an authentication token for them:
  *  {user: { username, firstName, lastName, email, isAdmin }, token }
  *
- * Authorization required: login
+ * Authorization required: login, admin
  **/
 
-router.post("/", ensureLoggedIn, async function (req, res, next) {
-  const validator = jsonschema.validate(
-    req.body,
-    userNewSchema,
-    {required: true}
-  );
+router.post("/", ensureLoggedIn, ensureAdmin, async function (req, res, next) {
+  const validator = jsonschema.validate(req.body, userNewSchema, {
+    required: true,
+  });
   if (!validator.valid) {
-    const errs = validator.errors.map(e => e.stack);
+    const errs = validator.errors.map((e) => e.stack);
     throw new BadRequestError(errs);
   }
 
@@ -43,32 +44,31 @@ router.post("/", ensureLoggedIn, async function (req, res, next) {
   return res.status(201).json({ user, token });
 });
 
-
 /** GET / => { users: [ {username, firstName, lastName, email }, ... ] }
  *
  * Returns list of all users.
  *
- * Authorization required: login
+ * Authorization required: login, admin
  **/
 
-router.get("/", ensureLoggedIn, async function (req, res, next) {
+router.get("/", ensureLoggedIn, ensureAdmin, async function (req, res, next) {
   const users = await User.findAll();
   return res.json({ users });
 });
-
 
 /** GET /[username] => { user }
  *
  * Returns { username, firstName, lastName, isAdmin }
  *
- * Authorization required: login
+ * Authorization required: login, admin, or
  **/
 
-router.get("/:username", ensureLoggedIn, async function (req, res, next) {
-  const user = await User.get(req.params.username);
-  return res.json({ user });
-});
-
+router.get("/:username", ensureLoggedIn, ensureAdminOrCurrentUser,
+  async function (req, res, next) {
+    const user = await User.get(req.params.username);
+    return res.json({ user });
+  }
+);
 
 /** PATCH /[username] { user } => { user }
  *
@@ -80,31 +80,37 @@ router.get("/:username", ensureLoggedIn, async function (req, res, next) {
  * Authorization required: login
  **/
 
-router.patch("/:username", ensureLoggedIn, async function (req, res, next) {
-  const validator = jsonschema.validate(
-    req.body,
-    userUpdateSchema,
-    {required: true}
-  );
-  if (!validator.valid) {
-    const errs = validator.errors.map(e => e.stack);
-    throw new BadRequestError(errs);
+router.patch(
+  "/:username",
+  ensureLoggedIn,
+  ensureAdmin,
+  async function (req, res, next) {
+    const validator = jsonschema.validate(req.body, userUpdateSchema, {
+      required: true,
+    });
+    if (!validator.valid) {
+      const errs = validator.errors.map((e) => e.stack);
+      throw new BadRequestError(errs);
+    }
+
+    const user = await User.update(req.params.username, req.body);
+    return res.json({ user });
   }
-
-  const user = await User.update(req.params.username, req.body);
-  return res.json({ user });
-});
-
+);
 
 /** DELETE /[username]  =>  { deleted: username }
  *
  * Authorization required: login
  **/
 
-router.delete("/:username", ensureLoggedIn, async function (req, res, next) {
-  await User.remove(req.params.username);
-  return res.json({ deleted: req.params.username });
-});
-
+router.delete(
+  "/:username",
+  ensureLoggedIn,
+  ensureAdmin,
+  async function (req, res, next) {
+    await User.remove(req.params.username);
+    return res.json({ deleted: req.params.username });
+  }
+);
 
 module.exports = router;
